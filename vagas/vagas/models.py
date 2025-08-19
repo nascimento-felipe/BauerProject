6dca80ccb9176542a2d532b2004d3d6649457bb9
Felipe de Andrade <nascimentofelipe565@gmail.com>
2025-08-19 12:21:13 -0300

    Primeira versão do banco

---
+++ vagas/vagas/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+class Vaga(models.Model):
+
+  titulo = models.CharField(max_length=100)
+  setor = models.CharField(max_length=100)
+  data_abertura = models.DateField(auto_now_add=True)
+  
+  STATUS_CHOICES = (
+    ('aberta', 'Aberta'),
+    ('fechada', 'Fechada'),
+  )
+
+  status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+
+class Candidato(models.Model):
+
+  nome = models.CharField(max_length=100)
+  e_mail = models.EmailField(unique=True)
+  data_nascimento = models.DateField()
+  experiencia = models.TextField()
+
+  vagas = models.ManyToManyField(Vaga, through='Candidatura')
+
+class Candidatura(models.Model):
+
+  candidato = models.ForeignKey(Candidato, on_delete=models.CASCADE)
+  vaga = models.ForeignKey(Vaga, on_delete=models.CASCADE)
+  data = models.DateField(auto_now_add=True)
+
+  STATUS_CANDIDATURA_CHOICES = (
+    ('pendente', 'Pendente'),
+    ('aprovada', 'Aprovada'),
+    ('rejeitada', 'Rejeitada'),
+  )
+
+  status_candidatura = models.CharField(max_length=20, choices=STATUS_CANDIDATURA_CHOICES)
